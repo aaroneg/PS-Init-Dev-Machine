2df3f117c9db75b673a3f78b8fd0e11d68e4c3df
Aaron <aaroneg@users.noreply.github.com>
2021-05-05 15:23:52 -0500

    Create create-workstation-1.ps1

---
+++ create-workstation-1.ps1
@@ -0,0 +1,6 @@
+# run this after a reboot
+# Download the WSL2 kernel
+start https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi
+
+# Set wsl to version 2
+wsl --set-default-version 2
